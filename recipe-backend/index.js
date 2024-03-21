24e3713f4b52a54479603dcae44e96917dc12d60
AARTHI-m32 <aarthi.m2022cse@sece.ac.in>
2024-03-21 15:55:55 +0530

    changes

--- recipe-backend/index.js
+++ recipe-backend/index.js
@@ -2,11 +2,11 @@ const mongoose = require('mongoose')
 const express = require('express')
 const Recipe = require('./Schema.js')
 const bodyParser = require('body-parser')
-
+const cors=require('cors')
 
 const app = express()
 app.use(bodyParser.json())
-
+app.use(cors())
 
 
 async function connectToDb() {
